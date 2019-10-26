010a66144e95297ea44fc6082c1f3cdc9a56821d
Shou C <scf@ieee.org>
2019-10-25 19:52:54 -0700

    staticify user controller

--- controller/user/user.go
+++ controller/user/user.go
@@ -1,19 +1,28 @@
 package user
 
 import (
+	"github.com/gin-gonic/gin"
 	"github.com/shouc/gbrm/database"
 	"github.com/shouc/gbrm/database/model"
 	"github.com/shouc/gbrm/mail"
 	"github.com/shouc/gbrm/utils"
-	"github.com/gin-gonic/gin"
 	"math/rand"
+	"net/http"
 	"strings"
 	"time"
 )
 
-func UserLogin(c *gin.Context) {
+// Views
+
+func GetLogin(c *gin.Context) {
+	c.HTML(http.StatusOK, "login", gin.H{
+		"title": "Login | GBRM",
+	})
+}
+
+func PostLogin(c *gin.Context) {
 	var userParam string
-	userParam = c.PostForm("user_param")
+	userParam = c.PostForm("email")
 	var password string
 	password = utils.Encrypt(c.PostForm("password"))
 	var cursor = database.GetDB()
@@ -26,25 +35,114 @@ func UserLogin(c *gin.Context) {
 	cursor.
 		Table("users").
 		Where("password = ?", password).
-		Where("name = ?", userParam).
 		Or("email = ?", userParam).
 		Limit("1").
 		Scan(&result)
 	if result.Id == 0 {
-		c.Set("success", 0)
-		c.Set("message", "Cannot find user!")
+		c.HTML(http.StatusOK, "login", gin.H{
+			"title":   "Login | GBRM",
+			"message": "Cannot find user!",
+		})
 	} else {
 		result.Session = utils.AddSession(result.Token)
-		c.Set("success", 1)
-		c.Set("result", result)
+		c.Redirect(http.StatusFound, "home?session="+result.Session)
 	}
 }
+
+func GetRegister(c *gin.Context) {
+	c.HTML(http.StatusOK, "register", gin.H{
+		"title": "Register | GBRM",
+	})
+}
+
+func PostRegister(c *gin.Context) {
+	var email string
+	email = c.PostForm("email")
+	if c.PostForm("password") != c.PostForm("confirm_password") {
+		c.HTML(http.StatusOK, "register", gin.H{
+			"title":   "Register | GBRM",
+			"message": "Password Not Match",
+			"email":   email,
+		})
+	}
+	var password string
+	password = utils.Encrypt(c.PostForm("password"))
+
+	// Insert Section
+	user := model.Users{
+		Email:    email,
+		Password: password,
+		IsAuth:   false,
+		Type:     1,
+		ProTill:  0}
+	var cursor = database.GetDB()
+	insertErr := cursor.Create(&user)
+	if insertErr.RowsAffected == 1 {
+		addAuthCode(email)
+		c.Redirect(http.StatusFound, "confirm?email="+user.Email)
+	} else {
+		errMessage := insertErr.Error.Error()
+		var message string
+		if strings.Contains(errMessage, "key 'email'") {
+			message = "Email has already been taken!"
+		} else {
+			message = "System Error"
+		}
+		c.HTML(http.StatusOK, "register", gin.H{
+			"title":   "Register | GBRM",
+			"message": message,
+			"email":   email,
+		})
+	}
+}
+
+func GetConfirm(c *gin.Context) {
+	userMail := c.Query("email")
+	c.HTML(http.StatusOK, "confirm", gin.H{
+		"title": "Confirm Your Email | GBRM",
+		"email": userMail,
+	})
+}
+
+func PostConfirm(c *gin.Context) {
+	var authCode = c.DefaultPostForm("auth_code", "0")
+	var email = c.DefaultPostForm("email", "0")
+
+	var rc = database.GetRedis()
+	authCodeSaved, _ := rc.Get(email).Result()
+	if authCodeSaved == "" {
+		c.HTML(http.StatusOK, "confirm", gin.H{
+			"title":   "Confirm | GBRM",
+			"email":   email,
+			"message": "The auth code is already expired or already used",
+		})
+	} else {
+		if authCodeSaved == authCode {
+			var token, session = utils.AddSessionWithoutToken()
+			var user model.Users
+			cursor := database.GetDB()
+			cursor.Model(&user).
+				Where("email = ?", email).
+				Updates(model.Users{Token: token, IsAuth: false})
+			_, _ = rc.Del(email).Result()
+			c.Redirect(http.StatusFound, "home?session="+session)
+		} else {
+			c.HTML(http.StatusOK, "confirm", gin.H{
+				"title":   "Confirm | GBRM",
+				"email":   email,
+				"message": "Incorrect Auth Code",
+			})
+		}
+	}
+}
+
+// API
 
 func UserInfo(c *gin.Context) {
 	var session string
 	session = c.Query("session")
-	type resultStruct struct{
-		Type int `json:"type"`
+	type resultStruct struct {
+		Type   int  `json:"type"`
 		IsPaid bool `json:"is_paid"`
 	}
 	var user = utils.RetrieveSession(session)
@@ -65,68 +163,4 @@ func addAuthCode(email string) string {
 	mail.SendAuthCode(authCode, email)
 	_ = rc.Set(email, authCode, time.Hour).Err()
 	return string(authCode)
-}
-
-func UserRegister(c *gin.Context) {
-	var email string
-	email = c.PostForm("email")
-	var username string
-	username = c.PostForm("username")
-	var password string
-	password = utils.Encrypt(c.PostForm("password"))
-
-	//Check Section
-
-	// Insert Section
-	user := model.Users{
-		Name:     username,
-		Email:    email,
-		Password: password,
-		IsAuth:   false,
-		Type:     1,
-		ProTill:  0}
-	var cursor = database.GetDB()
-	insertErr := cursor.Create(&user)
-	if insertErr.RowsAffected == 1 {
-		addAuthCode(email)
-		c.Set("success", 1)
-		c.Set("message", "Check your email")
-	} else {
-		errMessage := insertErr.Error.Error()
-		c.Set("success", 0)
-		if strings.Contains(errMessage, "key 'name'") {
-			c.Set("message", "Username has already been taken!")
-		} else if strings.Contains(errMessage, "key 'email'") {
-			c.Set("message", "Email has already been taken!")
-		} else {
-			c.Set("message", "System Error")
-		}
-	}
-}
-
-func UserConfirm(c *gin.Context) {
-	var authCode = c.DefaultPostForm("auth_code", "0")
-	var email = c.DefaultPostForm("email", "0")
-
-	var rc = database.GetRedis()
-	authCodeSaved, _ := rc.Get(email).Result()
-	if authCodeSaved == "" {
-		c.Set("success", 0)
-		c.Set("message", "The auth code is already expired or already used")
-	} else {
-		if authCodeSaved == authCode {
-			var token, session = utils.AddSessionWithoutToken()
-			var user model.Users
-			cursor := database.GetDB()
-			cursor.Model(&user).
-				Where("email = ?", email).
-				Updates(model.Users{Token: token, IsAuth: false})
-			_, _ = rc.Del(email).Result()
-			c.Set("success", 1)
-			c.Set("result", session)
-		} else {
-			c.Set("success", 0)
-			c.Set("message", "Incorrect Auth Code")
-		}
-	}
 }
